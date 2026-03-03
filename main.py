5be88971ca58ce9d358afaf6a6006d6733ff239c
komogorcevala-eng <komogorceva.la@gmail.com>
2026-03-03 20:57:13 +0300

    Рабочий бот

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import logging
-git add .from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from datetime import datetime
 from env import BOT_TOKEN, ADMIN_CHAT_ID, GOOGLE_DRIVE_ROOT_FOLDER_ID
 from google_drive_oauth import drive_service
